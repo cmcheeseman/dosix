3b66b8401ebb4ebf917cf262a48913cf47fd38d2
cmcheeseman <claytonmfrohoff@gmail.com>
2024-02-12 13:17:06 -0600

    improved documentation

--- defs.asm
+++ defs.asm
@@ -7,6 +7,12 @@ userloc: equ 0x8a00
 fdt: equ 0x8200
 
 ; kernel functions
+
+; it's printf
 printf: equ 0x01
+
+; prints a string with a fixed size. does not have formaterss
 printfs: equ 0x02
+
+; compare strings in bx and si, size is stored in ch, returns 0 in al if strings are the same
 strcmp: equ 0x03
