5c2733386194279bfbbab8e2f591dd05f42767bb
cmcheeseman <claytonmfrohoff@gmail.com>
2024-03-07 13:51:32 -0600

    added ls to dfsm

--- file-manager/dfsm.c
+++ file-manager/dfsm.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <string.h>
 
 int verifybootable(char *filename) {
   FILE *image = fopen(filename, "rb");
@@ -23,15 +24,39 @@ int verifybootable(char *filename) {
   }
 }
 
+int ls(char *filename) {
+  FILE *image = fopen(filename, "rb");
+  uint8_t *bytes = malloc(7 * 512);
+  if ((image == NULL) || (bytes == NULL)) {
+    printf("failed to read file\n");
+    free(bytes);
+    fclose(image);
+  }
+  fread(bytes, 3584, 1, image);
+  int i = 0x600;
+  while (i < 0xe00) {
+    if (bytes[i + 1] == 0) {
+      break;
+    }
+    printf("%.*s\n", 5, (char *)&bytes[i + 3]);
+    i += 9;
+  }
+  return 0;
+}
+
 int main(int argc, char *argv[]) {
   if (argc < 2) {
     printf("dfsm [FILENAME]\n");
     return -1;
   }
-  if (verifybootable(argv[1]) == 0) {
-    printf("the file is bootable\n");
+  if (strcmp(argv[1], "-vb") == 0) {
+    if (verifybootable(argv[2]) == 0) {
+      printf("the file is bootable\n");
+    } else {
+      printf("the file is not bootable\n");
+    }
   } else {
-    printf("the file is not bootable\n");
+    ls(argv[2]);
   }
   return 0;
 }
